7fdd3041c48269150cb0b7d9b163176c3a11fa39
unoValencia <valenciajg@students.nu-lipa.edu.ph>
2024-11-02 21:37:17 +0800

    Initial commit

---
+++ Includes/header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav class="container">
+        <div class="nav-left">
+            <a href="index.php" title="Click here to return to the homepage">Home</a>
+            <a href="about.php" title="View information about our organization">About</a>
+            <a href="contact.php" title="View our contact information">Contact</a>
+        </div>
+        <div class="nav-right">
+            <a href="login.php" title="Click here to login">Login</a>
+            <a href="signup.php" title="Click here to sign up">Sign Up</a>
+        </div>
+    </nav>
+</header>
